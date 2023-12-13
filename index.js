4b9fa74ce6b5e769fbecd65da7fe7e9ee97a26cc
Ahmad Ali khan <ahmadalikhan6534@gmail.com>
2023-12-13 16:36:14 +0500

    aaa

---
+++ index.js
@@ -0,0 +1,48 @@
+// const app = require('./app')
+// const fs = require('fs')
+// const server = require('http')
+// var a = 1;
+// var b = 2;
+// var c = a+b;
+// console.log(c)
+// for(i=5;i<=15;i++){
+//     console.log(i)
+// }
+// const array=[1,2,3,4,4,5,5,6,7]
+// const arr =     array.filter((item)=>{
+//     return item>4;
+    
+// })
+// console.log(arr)
+
+
+
+// console.log(app.x)
+
+
+// // moduke 
+
+// console.log('hello world')
+// fs.writeFileSync('first.js','hello')
+
+
+// // CREATE SERVER
+
+// const response =(req,resp)=>{
+// resp.write('<h1>response two</h1>');
+// resp.end();
+// }
+// server.createServer(response).listen(4800);
+
+
+// ARRAY filter
+
+const first = require('./first')
+
+// const array = [1,2,2,8,8,9,6,5]
+// const arr=array.filter((item)=>{
+//     return item===2
+// })
+// console.log(arr)
+
+console.log(first.a)
