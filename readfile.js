da13917a8a158384e5218183c73c9f0e0e50a95a
Ahmad Ali khan <ahmadalikhan6534@gmail.com>
2024-01-02 17:22:09 +0500

    eerre

--- readfile.js
+++ readfile.js
@@ -1,48 +1,58 @@
-const fs = require('fs')
-const path = require('path')
-const dirpath = path.join(__dirname,'file')
-const filepath = `${dirpath}/apple.js`
-fs.writeFileSync(filepath,'helooo world')
-
-// READ FILE
+// const fs = require('fs')
+// const path = require('path')
+// const dirpath = path.join(__dirname,'file')
+// const filepath = `${dirpath}/apple.js`
+// fs.writeFileSync(filepath,'helooo world')
 
 
+// // READ FILE
+
+// fs.readFile(filepath,'utf8',(err,item)=>{
+//     console.log(item)
+// })
+
+// // UPDATE FILE
+
+// fs.appendFile(filepath,'helo pakistan',(err)=>{
+//     if(err){
+//         console.log('err id genrated')
+//     }
+//     else{
+//         console.log('file is updated')
+//     }
+// })
+
+// // RENAME FILE
+
+// fs.rename(filepath,`${dirpath}/fruit.js`,(err)=>{
+//     if(err){
+//         console.log('errr found')
+//     }
+//     else{
+//         console.log('name is updated')
+//     }
+// })
 
 
-fs.readFile(filepath,'utf8',(err,item)=>{
-    console.log(item)
-})
+// let a = 10
+// let b = 0 
 
-// UPDATE FILE
+// const waitingdata= new Promise((item,resolve)=>{
+//     setTimeout(()=>{
+//         item(30)
+//     },3000)
+// })
 
-
-fs.appendFile(filepath,'helo pakistan',(err)=>{
-    if(err){
-        console.log('err id genrated')
-    }
-    else{
-        console.log('file is updated')
-    }
-})
-
-// RENAME FILE
+// waitingdata.then((data)=>{
+//     console.log(a+data)
+// })
 
 
-
-fs.rename(filepath,`${dirpath}/fruit.js`,(err)=>{
-    if(err){
-        console.log('errr found')
-    }
-    else{
-        console.log('name is updated')
-    }
-})
-
-
-let a = 10
-let b = 0
 console.log('first')
 setTimeout(()=>{
-   let b = 30
+    console.log('second')
 },2000)
-console.log(a+b)
+setTimeout(()=>{
+    console.log('third')
+},0)
+console.log('four')
