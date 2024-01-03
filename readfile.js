51a43c0f4bd8c023cbe7df2c211b3f22ba0d26e5
Ahmad Ali khan <ahmadalikhan6534@gmail.com>
2024-01-03 17:29:30 +0500

    jjjj

--- readfile.js
+++ readfile.js
@@ -1,58 +1,111 @@
+// // // const fs = require('fs')
+// // // const path = require('path')
+// // // const dirpath = path.join(__dirname,'file')
+// // // const filepath = `${dirpath}/apple.js`
+// // // fs.writeFileSync(filepath,'helooo world')
+
+
+// // // // READ FILE
+
+// // // fs.readFile(filepath,'utf8',(err,item)=>{
+// // //     console.log(item)
+// // // })
+
+// // // // UPDATE FILE
+
+// // // fs.appendFile(filepath,'helo pakistan',(err)=>{
+// // //     if(err){
+// // //         console.log('err id genrated')
+// // //     }
+// // //     else{
+// // //         console.log('file is updated')
+// // //     }
+// // // })
+
+// // // // RENAME FILE
+
+// // // fs.rename(filepath,`${dirpath}/fruit.js`,(err)=>{
+// // //     if(err){
+// // //         console.log('errr found')
+// // //     }
+// // //     else{
+// // //         console.log('name is updated')
+// // //     }
+// // // })
+
+
+// // // let a = 10
+// // // let b = 0 
+
+// // //
+
+
+// // console.log('first')
+// // setTimeout(()=>{
+// //     console.log('second')
+// // },2000)
+// // setTimeout(()=>{
+// //     console.log('third')
+// // },0)
+// // console.log('four')
+
 // const fs = require('fs')
 // const path = require('path')
 // const dirpath = path.join(__dirname,'file')
-// const filepath = `${dirpath}/apple.js`
-// fs.writeFileSync(filepath,'helooo world')
+// const filepath = `${dirpath}/apletwo.js`
+// fs.writeFileSync(filepath,'helo')
 
 
-// // READ FILE
+
+// fs.appendFile(filepath,'pakistan',(err)=>{
+//     if(!err){
+//         console.log('updated')
+//     }
+// })
 
 // fs.readFile(filepath,'utf8',(err,item)=>{
-//     console.log(item)
+   
+//         console.log(item)
+ 
 // })
 
-// // UPDATE FILE
-
-// fs.appendFile(filepath,'helo pakistan',(err)=>{
-//     if(err){
-//         console.log('err id genrated')
-//     }
-//     else{
-//         console.log('file is updated')
+// fs.rename(filepath,`${dirpath}/aplethree.js`,(err,item)=>{
+//     if(!err){
+// console.log('name updated')
 //     }
 // })
 
-// // RENAME FILE
-
-// fs.rename(filepath,`${dirpath}/fruit.js`,(err)=>{
-//     if(err){
-//         console.log('errr found')
-//     }
-//     else{
-//         console.log('name is updated')
-//     }
-// })
 
 
 // let a = 10
-// let b = 0 
+// let b = 30
+// console.log(a+b)
+// setTimeout(()=>{
+//  b= 30
+//  console.log(a+b)
+// },2000)
 
+
+// let a = 10
+// let b = 0
 // const waitingdata= new Promise((item,resolve)=>{
-//     setTimeout(()=>{
-//         item(30)
-//     },3000)
-// })
-
-// waitingdata.then((data)=>{
-//     console.log(a+data)
-// })
-
+//        setTimeout(()=>{
+//            item(30)
+//          },3000)
+//      })
+    
+//     waitingdata.then((data)=>{
+//          console.log(a+data)
+//  })
 
 console.log('first')
+
 setTimeout(()=>{
-    console.log('second')
+    console.log("second")
 },2000)
+
 setTimeout(()=>{
-    console.log('third')
+    console.log("three")
 },0)
-console.log('four')
+
+console.log("four")
